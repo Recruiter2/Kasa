b084c0c4a7584339311a9489c2757829b727b028
tim <timour1712@gmail.com>
2024-03-20 23:40:41 +0100

    added about collapse added link to description + css

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,19 +3,19 @@ import './about.css';
 import Banner from '../../components/Banner/Banner';
 import imgbanner from '../../assets/images/seaside-cliffs.png';
  import Collapse from '../../components/Collapse/Collapse';
+ import data from '../../assets/about.json';
+ 
 const About = () => {
-
-  const collapses = [
-    { title: "Fiabilité", content: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." },
-    { title: "Respect", content: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraîneraune exclusion de notre plateforme." },
-    { title: "Service", content: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraîneraune exclusion de notre plateforme." },
-    { title: "Sécurité", content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standarts sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." },
-  ];
 
   return (
     <main>
       < Banner src={imgbanner}  alt="Une image vue sur les sapins et les montagnes"  />
-      <Collapse items={collapses} />
+
+      <ul className='about-collapse'>
+      {data.map((about) => (
+        <Collapse  title={about.title} content={about.content} />
+      ))}
+      </ul>
 
     </main>
   )
