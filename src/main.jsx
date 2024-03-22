30d4e7d613cdf851ff6932953e4523bb7827d971
tim <timour1712@gmail.com>
2024-03-22 21:52:38 +0100

    rm old attempts

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-/* import { createBrowserRouter,   RouterProvider } from "react-router-dom";
- */
+
 import './index.css'
 import App from './App'
-/* import Home from "./pages/Home/Home";
 
-import ErrorPage from "./pages/Error/Error"; */
-//importer toutes les pages
-
-
-/* const router = createBrowserRouter([
- 
-  {
-    path: "/",
-    element: <Home />,
-  },
-
-  
-  {
-    path: "*",
-    element: <ErrorPage />,
-  },
-
-]);
- */
-
-
-/* ajouter navbar avant router 
-et footer apres le main*/
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
